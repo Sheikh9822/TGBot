acacb0e096ab250261fbab1270fe0c8f6c5893b0
ＭΛＬＩＴＵＳ <zubairsheikh9957@gmail.com>
2026-02-08 12:08:24 +0530

    Update and rename main.py to app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+from flask import Flask
+import os
+app = Flask(__name__)
+@app.route('/')
+def health(): return "Bot is running!", 200
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))

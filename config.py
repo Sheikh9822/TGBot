0192f314d2939a35065d5248f8965b71db5753a8
ＭΛＬＩＴＵＳ <zubairsheikh9957@gmail.com>
2026-02-09 12:04:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,17 @@ import os
 API_ID = int(os.environ.get("API_ID", 0))
 API_HASH = os.environ.get("API_HASH", "")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", "")
-INDEX_URL = os.environ.get("INDEX_URL", "").rstrip('/')
-DUMP_CHAT_ID = int(os.environ.get("DUMP_CHAT_ID", 0))
 
-# OAuth Individual Keys
+# GDrive OAuth Config
 CLIENT_ID = os.environ.get("CLIENT_ID", "")
 CLIENT_SECRET = os.environ.get("CLIENT_SECRET", "")
 REFRESH_TOKEN = os.environ.get("REFRESH_TOKEN", "")
+GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", "")
+INDEX_URL = os.environ.get("INDEX_URL", "").rstrip('/')
+
+# Telegram DB Config
+DUMP_ID_RAW = os.environ.get("DUMP_CHAT_ID", "0")
+try:
+    DUMP_CHAT_ID = int(DUMP_ID_RAW)
+except ValueError:
+    DUMP_CHAT_ID = DUMP_ID_RAW

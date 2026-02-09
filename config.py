edfe52eee8ab2de2d0be6079cd8bc3b61d4d5661
ＭΛＬＩＴＵＳ <zubairsheikh9957@gmail.com>
2026-02-09 11:50:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 import os
-import base64
 
 API_ID = int(os.environ.get("API_ID", 0))
 API_HASH = os.environ.get("API_HASH", "")
@@ -8,9 +7,7 @@ GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", "")
 INDEX_URL = os.environ.get("INDEX_URL", "").rstrip('/')
 DUMP_CHAT_ID = int(os.environ.get("DUMP_CHAT_ID", 0))
 
-# OAuth Token Handling
-GDRIVE_TOKEN_BASE64 = os.environ.get("GDRIVE_TOKEN_BASE64", "")
-
-if GDRIVE_TOKEN_BASE64:
-    with open('token.pickle', 'wb') as f:
-        f.write(base64.b64decode(GDRIVE_TOKEN_BASE64))
+# OAuth Individual Keys
+CLIENT_ID = os.environ.get("CLIENT_ID", "")
+CLIENT_SECRET = os.environ.get("CLIENT_SECRET", "")
+REFRESH_TOKEN = os.environ.get("REFRESH_TOKEN", "")
